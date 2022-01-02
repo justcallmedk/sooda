070e8562c12fbfa820cf90c45c1e656a2dadf9d6
DK <hi@justcallme.dk>
2022-01-01 21:21:37 -0500

    first commit

---
+++ src/components/Forecast.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import './Forecast.css';
+
+function Forecast(props) {
+  return (
+    <div class="forecast">
+      Hello, World
+    </div>
+  );
+}
+
+export default Forecast;
